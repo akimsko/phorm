9caed5c43eeffec3a34c7c0d9e318fefcaa1209d
Bo Thinggaard <akimsko@tnactas.dk>
2014-01-30 16:59:07 +0100

    Docs.

--- src/Phorm/Renderer/FileRenderer.php
+++ src/Phorm/Renderer/FileRenderer.php
@@ -1,18 +1,26 @@
 <?php
-/**
- * This file is part of the phorm project.
+
+/*
+ * This file is part of the Phorm project.
+ *
+ * @link https://github.com/akimsko/phorm
+ *
+ * @copyright Copyright 2014 Bo Thinggaard
+ * @license   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
  */
 
-
 namespace Phorm\Renderer;
-use Phorm\Resolver\FileResolver;
 
+use Phorm\Resolver\FileResolver;
 
 /**
  * Class FileRenderer
  *
  * @package Phorm\Renderer
- * @author Bo Thinggaard <bo@unpossiblesystems.com>
+ * @author  Bo Thinggaard <bo@unpossiblesystems.com>
  */
 abstract class FileRenderer extends Renderer {
 
--- src/Phorm/Resolver/FileResolver.php
+++ src/Phorm/Resolver/FileResolver.php
@@ -1,19 +1,27 @@
 <?php
-/**
- * This file is part of the phorm project.
+
+/*
+ * This file is part of the Phorm project.
+ *
+ * @link https://github.com/akimsko/phorm
+ *
+ * @copyright Copyright 2014 Bo Thinggaard
+ * @license   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
  */
 
-
 namespace Phorm\Resolver;
+
 use Phorm\Element\Element;
 use Phorm\Exception\ResolverException;
 
-
 /**
- * Class PhpResolver
+ * Class FileResolver
  *
  * @package Phorm\Resolver
- * @author Bo Thinggaard <bo@unpossiblesystems.com>
+ * @author  Bo Thinggaard <bo@unpossiblesystems.com>
  */
 class FileResolver implements Resolver {
 
