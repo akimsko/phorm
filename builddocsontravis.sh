ca84d7916e932717f1bf5ac23ecdc4f2aa6ad922
Bo Thinggaard <akimsko@tnactas.dk>
2014-03-13 15:16:38 +0100

    stuff

--- builddocsontravis.sh
+++ builddocsontravis.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 PHPV=`php -r "echo PHP_MINOR_VERSION;"`
 if [ "$PHPV" = "3" ]; then
-	pear channel-discover pear.phpdoc.org
-	pear install phpdoc/phpDocumentor-alpha
-	phpenv rehash
+	#pear channel-discover pear.phpdoc.org
+	#pear install phpdoc/phpDocumentor-alpha
+	#phpenv rehash
 	cd ~
 	git clone https://github.com/akimsko/phorm.wiki.git
 	cd phorm.wiki
@@ -11,14 +11,14 @@ if [ "$PHPV" = "3" ]; then
 	git config user.email "noreply@travis-ci.org"
 	git config credential.helper "store --file=.git/credentials"
 	echo "https://$GH_TOKEN:@github.com" > .git/credentials
-	git rm Api/*
-	mkdir Api
-	phpdoc parse -t . -d ~/build/akimsko/phorm/src --visibility=public
-	~/build/akimsko/phorm/vendor/evert/phpdoc-md/bin/phpdocmd --lt "%c" structure.xml Api
-	git add Api/Phorm-*
+	#git rm Api/*
+	#mkdir Api
+	#phpdoc parse -t . -d ~/build/akimsko/phorm/src --visibility=public
+	#~/build/akimsko/phorm/vendor/evert/phpdoc-md/bin/phpdocmd --lt "%c" structure.xml Api
+	#git add Api/Phorm-*
 	git commit -m "Updated documentation." && git push
 	rm ~/.git/credentials
-	sleep 20
+	sleep 180
 else
 	echo " * Only building docs on PHP 5.3 - Not on 5.$PHPV"
 fi
